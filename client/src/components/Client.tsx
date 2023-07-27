b9e12d8c3f16159009ff94e81edc2639191d99e7
Adam Griffiths <107414001+adamgriff86@users.noreply.github.com>
2023-07-27 20:27:54 +0200

    feat: styling supplier component, client

--- client/src/components/Client.tsx
+++ client/src/components/Client.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import SupplierComponent from './SupplierComponent';
+import '../styles/client.css'
 
 export default function Client() {
   return (
@@ -9,10 +10,16 @@ export default function Client() {
         <h1>Shenanigans</h1>
       </header>
       <main>
-        <SupplierComponent name='Beer Dewds' />
-        <SupplierComponent name='Victorias Veggies' />
-        <SupplierComponent name='Sick Spirits' />
-        <SupplierComponent name='Meat Munchers' />
+        <div className='supplier-list-container'>
+          <SupplierComponent name='Beer Dewds' />
+          <SupplierComponent name='Victorias Veggies' />
+          <SupplierComponent name='Sick Spirits' />
+          <SupplierComponent name='Meat Munchers' />
+        </div>
+        <div className='flex-row margin-30-top'>
+          <div className='btn blue'>Stocktake</div>
+          <div className='btn black'>Add Supplier</div>
+        </div>
       </main>
     </>
   );
