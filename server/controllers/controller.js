5fd42028bfc1372d6cebdfc677ea5f136dadf5ae
Adam Griffiths <107414001+adamgriff86@users.noreply.github.com>
2023-07-27 10:41:42 +0200

    feat: prisma create queries

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,52 +1,97 @@
-// import the Prisma client
-// instantiate it with the connection details for the database
-// to perform database operations
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+// import the Prisma client from the index.js file
+const prisma = require('../index');
 
-//use async functions to perform database operations
-async function main() {
+// SUPPLIER QUERIES
+
+async function getSuppliers(req, res) {
   try {
-    await prisma.$connect();
-    console.log('connected to database');
-    const newSupplier = await prisma.supplier.create({
-      data: {
-        companyName: 'Beer Dewds',
-        email: 'orders@beerdewds.com',
-        contactName: 'Benjamin Ramjan',
-      },
-    });
-    console.log(newSupplier);
+    const suppliers = await prisma.supplier.findMany();
+    res.status(200).send(suppliers);
   } catch (error) {
-    console.log(error, 'Error connecting to DB');
-  } finally {
-    await prisma.$disconnect();
+    res.status(400).send(`Error getting suppliers: ${error}`);
   }
 }
 
-main();
+async function createSupplier(req, res) {
+  try {
+    console.log(req.body);
+    const { companyName, email, contactName, clientId } = req.body;
+    if (!companyName || !email || !contactName || !clientId)
+      res.status(400).send('Incomplete fields.');
 
-// async function getSuppliers (req, res) {
-//   try {
-//     const suppliers = await prisma.supplier.findMany();
-//     console.log(suppliers);
-//     res.send(suppliers);
-//   } catch (error) {
-//     res.status(400).send(`Error getting suppliers: ${error}`);
-//   }
-// };
+    const foundSupplier = await prisma.supplier.findUnique({
+      where: {
+        companyName: companyName,
+      },
+    });
+    if (foundSupplier)
+      res.status(404).send('Supplier already exists: ', foundSupplier);
+    const newSupplier = await prisma.supplier.create({
+      data: {
+        companyName: companyName,
+        email: email,
+        contactName: contactName,
+        clientId: clientId,
+      },
+    });
+    res.status(201).send('New supplier created!', newSupplier);
+  } catch (error) {
+    res.status(400).send(`Error creating new supplier: ${error}`);
+  }
+}
 
-// async function createSupplier() {
-//   try {
-//     const newSupplier = await prisma.supplier.create({
-//       data: {
-//         companyName: "Beer Dewds",
-//         email: "orders@beerdewds.com",
-//         contactName: "Benjamin Ramjan"
-//       },
-//     })
-//     res.status(201).send(newSupplier);
-//   } catch (error) {
-//     res.status(400).send(`Error creating new supplier: ${error}`);
-//   }
-// }
+// CLIENT QUERIES
+
+async function createClient(req, res) {
+  try {
+    console.log(req.body);
+    const { companyName, email, contactName } = req.body;
+    if (!companyName || !email || !contactName)
+      res.status(400).send('Incomplete fields.');
+    const foundClient = await prisma.client.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    if (foundClient) res.status(404).send('Client already exists');
+    const newClient = await prisma.client.create({
+      data: {
+        companyName: companyName,
+        email: email,
+        contactName: contactName,
+      },
+    });
+    res.status(201).send('New client created!', newClient);
+  } catch (error) {
+    res.status(400).send(`Error creating new supplier: ${error}`);
+  }
+}
+
+// PRODUCT QUERIES
+async function createProduct(req, res) {
+  try {
+    console.log(req.body);
+    const { supplierId, productId, name, price } = req.body;
+    if (!supplierId || !name || !price)
+      res.status(400).send('Incomplete fields.');
+
+    const newClient = await prisma.product.create({
+      data: {
+        supplierId: supplierId,
+        productId: productId,
+        name: name,
+        price: price,
+      },
+    });
+    res.status(201).send('New client created!', newClient);
+  } catch (error) {
+    res.status(400).send(`Error creating new supplier: ${error}`);
+  }
+}
+
+module.exports = {
+  getSuppliers,
+  createSupplier,
+  createClient,
+  createProduct,
+};
--- server/index.js
+++ server/index.js
@@ -2,16 +2,37 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const router = require('./router');
+const { PrismaClient } = require('@prisma/client');
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
+const prisma = new PrismaClient();
+
 app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(port, () => {
-  console.log(`🚀 Server running at http://localhost:${port} 🛰`);
-});
+(async () => {
+  try {
+    //connect to database
+    await prisma.$connect();
+    console.log('connected to database.');
+
+    // run server
+    app.listen(port, () => {
+      console.log(`🚀 Server running at http://localhost:${port} 🛰`);
+    });
+
+    app.on('close', () => {
+      prisma.$disconnect();
+      console.log('Disconnected from databse.');
+    });
+  } catch (error) {
+    console.log('Error connecting to DB.', error);
+  }
+})();
+
+module.exports = { prisma }
