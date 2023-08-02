a025cf930b0442e6df2244eaf9ae56e63b8ebce2
Adam Griffiths <107414001+adamgriff86@users.noreply.github.com>
2023-08-02 16:17:27 +0200

    revert

--- server/controllers/suppliercontroller.js
+++ server/controllers/suppliercontroller.js
@@ -3,11 +3,6 @@ import { getSupplierDetails } from '../models/supplierModel.js';
 
 export async function getSuppliers(req, res) {
   try {
-<<<<<<< HEAD:server/controllers/supplier.controller.js
-    const id = req.params.clientId;
-    const result = await supplier.getSuppliersByClient(id);
-    res.status(200).send(result);
-=======
     const { client } = req.params.clientId;
     const suppliers = await prisma.supplier.findMany({
       where: {
@@ -16,7 +11,6 @@ export async function getSuppliers(req, res) {
     });
 
     res.status(200).send(suppliers);
->>>>>>> parent of 41d1b6a (feat: modularise server):server/controllers/suppliercontroller.js
   } catch (error) {
     res.status(400).send(`Error getting suppliers: ${error}`);
   }
@@ -37,20 +31,6 @@ export async function createSupplier(req, res) {
     const { companyName, email, contactName, clientId } = req.body;
     if (!companyName || !email || !contactName || !clientId)
       res.status(400).send('Incomplete fields.');
-<<<<<<< HEAD:server/controllers/supplier.controller.js
-    const found = supplier.checkSupplierByEmail(email);
-    if (found) {
-      return res.status(404).send('Supplier already exists.');
-    }
-    console.log('data', { companyName, email, contactName, clientId });
-    const result = await supplier.createSupplier({
-      companyName,
-      email,
-      contactName,
-      clientId,
-    });
-    res.status(201).send(result);
-=======
     const foundSupplier = await prisma.supplier.findUnique({
       where: {
         companyName: companyName,
@@ -69,7 +49,6 @@ export async function createSupplier(req, res) {
       },
     });
     res.status(201).send(newSupplier);
->>>>>>> parent of 41d1b6a (feat: modularise server):server/controllers/suppliercontroller.js
   } catch (error) {
     res.status(400).send(`Error creating new supplier: ${error}`);
   }
@@ -79,18 +58,12 @@ export async function deleteSupplier(req, res) {
   // update the client supplier list and keeep the supplier in the DB!
   try {
     const { supplierId } = req.body;
-<<<<<<< HEAD:server/controllers/supplier.controller.js
-    console.log(supplierId);
-
-    const result = supplier.deleteSupplier(supplierId);
-=======
     const result = await prisma.supplier.delete({
       where: {
         id: supplierId,
       },
     });
 
->>>>>>> parent of 41d1b6a (feat: modularise server):server/controllers/suppliercontroller.js
     res.status(200).send(result);
   } catch (error) {
     res.status(400).send(`Error getting suppliers: ${error}`);
